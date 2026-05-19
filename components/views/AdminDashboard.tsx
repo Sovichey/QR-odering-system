82dbf7405052180221c59d041eecce0ff9d7813c
Sovichey <2024006rim@aupp.edu.kh>
2026-05-19 17:35:04 +0700

    update

--- components/views/AdminDashboard.tsx
+++ components/views/AdminDashboard.tsx
@@ -166,7 +166,7 @@ export function AdminDashboard({ onLogout }: AdminDashboardProps) {
             <CardTitle>Order History</CardTitle>
           </CardHeader>
           <CardContent>
-            <div className="w-full rounded-md border overflow-auto">
+            <div className="w-full rounded-md border">
               <div className="h-[400px] overflow-auto">
                 <table className="w-full min-w-max">
                   <thead className="sticky top-0 bg-background">
